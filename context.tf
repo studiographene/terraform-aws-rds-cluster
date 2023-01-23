30deeb9a603b7ffb14d6dc9c516eaaa26e7a682e
Jatin Singhal <62590660+jatinsinghalsg@users.noreply.github.com>
2023-01-23 15:10:57 +0530

    bumpped version for null label

--- context.tf
+++ context.tf
@@ -14,7 +14,7 @@
 
 module "this" {
   source  = "app.terraform.io/studiographene/sg-label/null"
-  version = "1.0.3"
+  version = "1.0.4"
 
   enabled             = var.enabled
   project_name        = var.project_name
